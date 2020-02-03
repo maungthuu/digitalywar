c7f725687c845645eabb8808a8bede5702eef464
Maung Thuu <maungthuu@users.noreply.github.com>
2020-02-03 14:45:37 +0630

    WP2Static

---
+++ wp-content/themes/blocksy/static/js/customizer/sync/woocommerce/content-single-product.js
@@ -0,0 +1,45 @@
+import { setRatioFor } from '../helpers'
+
+wp.customize('product_content_style', val =>
+	val.bind(to => {
+		if (!document.body.classList.contains('single-product')) {
+			return
+		}
+
+		const article = document.querySelector('.product.type-product')
+
+		article.dataset.structure = to
+	})
+)
+wp.customize('gallery_style', val =>
+	val.bind(to => {
+		if (!document.body.classList.contains('single-product')) {
+			return
+		}
+
+		const article = document.querySelector('.product.type-product')
+
+		article.classList.remove('thumbs-left')
+
+		if (to !== 'horizontal' && article.querySelector('.flexy-items')) {
+			article.classList.add('thumbs-left')
+		}
+	})
+)
+
+wp.customize('product_gallery_ratio', val =>
+	val.bind(to => {
+		if (!document.body.classList.contains('single-product')) {
+			return
+		}
+
+		const article = document.querySelector('.product.type-product')
+
+		let thumb_ratio = (to || '4/3').split('/')
+		;[
+			...article.querySelectorAll(
+				'.flexy-items .ct-image-container .ct-ratio, .ct-product-view > .ct-image-container .ct-ratio'
+			)
+		].map(el => setRatioFor(to, el))
+	})
+)
