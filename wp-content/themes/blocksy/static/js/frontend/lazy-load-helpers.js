910889f13c7688a48ee7a397c40723114c73d070
Maung Thuu <maungthuu@users.noreply.github.com>
2020-02-03 14:46:12 +0630

    WP2Static

---
+++ wp-content/themes/blocksy/static/js/frontend/lazy-load-helpers.js
@@ -0,0 +1,22 @@
+export const markImagesAsLoaded = el =>
+	el &&
+	[...el.querySelectorAll('.ct-image-container.ct-lazy')].map(el => {
+		if (!el.querySelector('img')) {
+			return
+		}
+
+		el.querySelector('img').setAttribute(
+			'src',
+			el.querySelector('img').dataset.lazy
+		)
+
+		if (el.querySelector('img').dataset.lazySet) {
+			el.querySelector('img').setAttribute(
+				'srcset',
+				el.querySelector('img').dataset.lazySet
+			)
+		}
+
+		el.classList.remove('ct-lazy')
+		el.classList.add('ct-lazy-loaded')
+	})
