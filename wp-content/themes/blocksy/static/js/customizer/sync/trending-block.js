69fa2d086b9dd202d32c54e99ff7f8d2ff2d2887
Maung Thuu <maungthuu@users.noreply.github.com>
2020-02-03 14:45:24 +0630

    WP2Static

---
+++ wp-content/themes/blocksy/static/js/customizer/sync/trending-block.js
@@ -0,0 +1,19 @@
+import { checkAndReplace } from './helpers'
+import { responsiveClassesFor } from './footer'
+import { markImagesAsLoaded } from '../../frontend/lazy-load-helpers'
+
+checkAndReplace({
+	id: 'has_trending_block',
+	parent_selector: '#main',
+	selector: '.ct-trending-block',
+	fragment_id: 'blocksy-trending-block',
+	strategy: 'maybeBefore:.ct-instagram-block',
+	watch: ['trending_block_visibility'],
+	whenInserted: () => {
+		const trending = document.querySelector('.ct-trending-block')
+
+		markImagesAsLoaded(trending)
+		ctEvents.trigger('ct:trending-block:mount')
+		responsiveClassesFor('trending_block_visibility', trending)
+	}
+})
